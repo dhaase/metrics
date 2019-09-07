95f7459aa4ea22fd2e30877bf6716a5770e1c1c9
dhaase <dhaase@users.noreply.github.com>
2019-09-07 16:29:51 +0200

    intermediate commit

--- src/main/java/eu/dirk/haase/heap/Allocator.java
+++ src/main/java/eu/dirk/haase/heap/Allocator.java
@@ -7,8 +7,8 @@ import java.nio.ByteBuffer;
 public final class Allocator {
 
     private static final short EMPTY_DATA_SIZE = (short) 0;
+    private static final short NO_BLOCK_POSITION = (short) -1;
     private static final short NO_DATA_POSITION = (short) -1;
-
     private final Block block;
     private final ByteBuffer buffer;
     private final int maximumSize;
@@ -28,48 +28,86 @@ public final class Allocator {
     }
 
     public Allocator(final int startPosition, final int maximumSize) {
-        this(ByteBuffer.allocateDirect(maximumSize), startPosition, maximumSize);
+        this(ByteBuffer.allocateDirect(startPosition + maximumSize), startPosition, maximumSize);
     }
 
     public static int calculateNeededSizeOverAll(final int dataSize) {
         return (dataSize + Block.blockSize);
     }
 
+    public static int headerSize() {
+        return Block.blockSize;
+    }
+
     public int allocate(final Struct struct) {
+        short lastNext = findFreeBlock(struct);
+
+        if (lastNext != NO_BLOCK_POSITION) {
+            this.block.setStructAbsolutePosition(lastNext);
+            return occupyCurrentFreeBlock(struct);
+        }
+
+        return NO_DATA_POSITION;
+    }
+
+    public short findFreeBlock(final Struct struct) {
         block.initByteBuffer(this.buffer, startPosition);
 
         short lastNext = block.next.get();
         lastNext = (short) (lastNext < startPosition ? startPosition : lastNext);
+        if (block.data.get() == EMPTY_DATA_SIZE) {
+            // first Block is free
+            lastNext = (short) startPosition;
+        } else {
+            lastNext = findNextFreeBlock(lastNext);
+        }
+
+        this.block.setStructAbsolutePosition(lastNext);
+        if ((lastNext >= startPosition)
+                && (block.data.get() == EMPTY_DATA_SIZE)
+                && (this.buffer.limit() >= (this.block.dataPosition() + struct.size()))) {
+            return lastNext;
+        }
+
+        return NO_BLOCK_POSITION;
+    }
+
+    private short findNextFreeBlock(short lastNext) {
         short currNext = lastNext;
         while ((currNext > startPosition) && (block.data.get() != EMPTY_DATA_SIZE)) {
             lastNext = currNext;
             this.block.setStructAbsolutePosition(lastNext);
             currNext = block.next.get();
         }
-
-        this.block.setStructAbsolutePosition(lastNext);
-        if ((lastNext >= startPosition) && (block.data.get() == EMPTY_DATA_SIZE)) {
-            final short dataPosition = (short) this.block.dataPosition();
-            final short newNext = (short) this.block.nextHeaderPosition(struct.size());
-            if (newNext <= (maximumSize + startPosition)) {
-                block.data.set(dataPosition);
-                if ((newNext < (maximumSize + startPosition)) && (this.buffer.limit() > (newNext + Block.blockSize))) {
-                    block.next.set(newNext);
-                }
-            }
-            print();
-            struct.initByteBuffer(this.buffer, dataPosition);
-            return dataPosition;
-        }
-
-        return NO_DATA_POSITION;
+        return lastNext;
     }
 
-    public void free(final int dataPosition) {
+    public boolean free(final int dataPosition) {
         final int headerAbsolutePosition = this.block.headerPosition(dataPosition);
         this.block.setStructAbsolutePosition(headerAbsolutePosition);
-        this.block.data.set(EMPTY_DATA_SIZE);
+        if (this.block.data.get() == dataPosition) {
+            this.block.data.set(EMPTY_DATA_SIZE);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isAvailable(final Struct struct) {
+        return (findFreeBlock(struct) != NO_BLOCK_POSITION);
+    }
+
+    private int occupyCurrentFreeBlock(Struct struct) {
+        final short dataPosition = (short) this.block.dataPosition();
+        final short newNext = (short) this.block.nextHeaderPosition(struct.size());
+        if (newNext <= (maximumSize + startPosition)) {
+            block.data.set(dataPosition);
+            if ((newNext < (maximumSize + startPosition)) && (this.buffer.limit() > (newNext + Block.blockSize))) {
+                block.next.set(newNext);
+            }
+        }
         print();
+        struct.initByteBuffer(this.buffer, dataPosition);
+        return dataPosition;
     }
 
     private void print() {
--- src/test/java/eu/dirk/haase/heap/AllocatorTest.java
+++ src/test/java/eu/dirk/haase/heap/AllocatorTest.java
@@ -11,6 +11,44 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class AllocatorTest {
 
     @Test
+    public void test_that() {
+        // Given
+        Allocator allocator = new Allocator(2048);
+        MyStruct struct = new MyStruct();
+        // When
+        int dataPosition1 = allocator.allocate(struct);
+        int dataPosition2 = allocator.allocate(struct);
+        int dataPosition3 = allocator.allocate(struct);
+        int dataPosition4 = allocator.allocate(struct);
+        // Then
+        assertThat(dataPosition1).isEqualTo((Allocator.headerSize() * 1));
+        assertThat(dataPosition2).isEqualTo((Allocator.headerSize() * 2) + (struct.size() * 1));
+        assertThat(dataPosition3).isEqualTo((Allocator.headerSize() * 3) + (struct.size() * 2));
+        assertThat(dataPosition4).isEqualTo((Allocator.headerSize() * 4) + (struct.size() * 3));
+    }
+
+    @Test
+    public void test_that_limit() {
+        // Given
+        final int startOffset = 123;
+        MyStruct struct = new MyStruct();
+        final int countStructs = 10;
+        int maximumSize = Allocator.calculateNeededSizeOverAll(struct.size()) * countStructs;
+        Allocator allocator = new Allocator(startOffset, maximumSize);
+        //
+        for (int i = 0; countStructs > i; ++i) {
+            // When
+            int dataPosition = allocator.allocate(struct);
+            struct.m_1_signed08.set((byte) i);
+            struct.m_2_float32.set((float) i);
+            // Then
+            assertThat(dataPosition).isEqualTo(startOffset + (Allocator.headerSize() * (i + 1)) + (struct.size() * i));
+        }
+        System.out.println(allocator.free(154));
+        System.out.println(allocator.allocate(struct));
+    }
+
+    @Test
     public void test_that_offset() {
         // Given
         final int startOffset = 123;
@@ -22,48 +60,10 @@ public class AllocatorTest {
         int dataPosition3 = allocator.allocate(struct);
         int dataPosition4 = allocator.allocate(struct);
         // Then
-        assertThat(dataPosition1).isEqualTo(4 + startOffset);
-        assertThat(dataPosition2).isEqualTo(13 + startOffset);
-        assertThat(dataPosition3).isEqualTo(22 + startOffset);
-        assertThat(dataPosition4).isEqualTo(31 + startOffset);
-    }
-
-    @Test
-    public void test_that() {
-        // Given
-        Allocator allocator = new Allocator(2048);
-        MyStruct struct = new MyStruct();
-        // When
-        int dataPosition1 = allocator.allocate(struct);
-        int dataPosition2 = allocator.allocate(struct);
-        int dataPosition3 = allocator.allocate(struct);
-        int dataPosition4 = allocator.allocate(struct);
-        // Then
-        assertThat(dataPosition1).isEqualTo(4);
-        assertThat(dataPosition2).isEqualTo(13);
-        assertThat(dataPosition3).isEqualTo(22);
-        assertThat(dataPosition4).isEqualTo(31);
-    }
-
-
-    @Test
-    public void test_that_limit() {
-        // Given
-        MyStruct struct = new MyStruct();
-        final int countStructs = 10;
-        int maximumSize = Allocator.calculateNeededSizeOverAll(struct.size()) * countStructs;
-        Allocator allocator = new Allocator(maximumSize);
-        //
-        for(int i=0; countStructs > i; ++i) {
-            // When
-            int dataPosition = allocator.allocate(struct);
-            struct.m_1_signed08.set((byte)i);
-            struct.m_2_float32.set((float) i);
-            // Then
-        }
-        allocator.free(13);
-        int dataPosition = allocator.allocate(struct);
-        System.out.println(dataPosition);
+        assertThat(dataPosition1).isEqualTo(startOffset + (Allocator.headerSize() * 1));
+        assertThat(dataPosition2).isEqualTo(startOffset + (Allocator.headerSize() * 2) + (struct.size() * 1));
+        assertThat(dataPosition3).isEqualTo(startOffset + (Allocator.headerSize() * 3) + (struct.size() * 2));
+        assertThat(dataPosition4).isEqualTo(startOffset + (Allocator.headerSize() * 4) + (struct.size() * 3));
     }
 
     static class MyStruct extends Struct {
